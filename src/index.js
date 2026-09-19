f7aa89f1ca52393217b37cbd6152785044e13d3b
Aleksandra <aleksandrapozarickaa@gmail.com>
2026-09-19 13:06:03 +0000

    Implement tab-switching logic in the index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-import { loadHome } from './home.js'
+import { loadHome } from "./home.js";
+import { loadMenu } from "./menu.js";
+import { loadAbout } from "./about.js";
+
+
+function clearContent() {
+  const content = document.getElementById("content");
+  content.innerHTML = "";
+}
+
+const homeBtn = document.getElementById("home-btn");
+const menuBtn = document.getElementById("menu-btn");
+const aboutBtn = document.getElementById("about-btn");
+
+homeBtn.addEventListener("click", () => {
+  clearContent();
+  loadHome();
+});
+
+menuBtn.addEventListener("click", () => {
+  clearContent();
+  loadMenu();
+});
+
+aboutBtn.addEventListener("click", () => {
+  clearContent();
+  loadAbout();
+});
 
 loadHome();
